e21a5ffbd440be56b674d5f3bb9f6fc537a95e97
kmr22live <Kmr22live@gmail.com>
2023-08-26 18:23:41 +0530

    add search button to hide and show search

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -8,10 +8,13 @@ import {
   Modal,
   Row,
   Stack,
+  Pagination,
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ReactSelect from "react-select";
 import { Tag } from "../App";
+import { useAppDispatch } from "../store/Store";
+import { logout } from "../store/AuthSlice";
 
 // type SimplifiedNote = {
 //   id: string;
@@ -51,7 +54,10 @@ export function NoteList({
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [title, setTitle] = useState("");
   const [editTagsModalIsOpen, setEditTagsModalIsOpen] = useState(false);
-  console.log("reduxnotes", notes);
+  const [searchOpen, setSearchOpen] = useState<boolean>(false);
+
+  const dispatch = useAppDispatch();
+  const nav = useNavigate();
 
   const filteredNotes = useMemo(() => {
     return notes.filter((note) => {
@@ -70,75 +76,103 @@ export function NoteList({
 
   return (
     <div id="app">
-      <Row className="align-items-center mb-4">
-        <Col>
-          <h1>Notes</h1>
-        </Col>
-        <Col xs="auto">
-          <Stack
-            gap={2}
-            direction="horizontal"
-            className="justify-content-center align-self-center"
-          >
-            <Link to="/new" className="text-decoration-none">
-              {/* <Button variant="primary">Create</Button> */}
-              <Form
+      <div>
+        <Row className="align-items-center">
+          <Col>
+            <h1>
+              <i className="fa-solid fa-note-sticky"></i> Notesapp
+            </h1>
+          </Col>
+          <Col xs="auto">
+            <Stack
+              gap={2}
+              direction="horizontal"
+              className="justify-content-center align-self-center"
+            >
+              <Button
                 onClick={() => {
-                  console.log("haoasdfasd");
+                  dispatch(logout());
+                  nav("/login");
                 }}
+                variant="dark"
               >
-                <Form.Group controlId="formGroupEmail">
+                Sign Out
+              </Button>
+            </Stack>
+          </Col>
+        </Row>
+        <Row className="align-items-center my-4">
+          <Col>
+            <Button onClick={() => setSearchOpen(!searchOpen)} variant="dark">
+              Search Notes
+            </Button>
+          </Col>
+          <Col xs="auto" className="mt-3">
+            <Stack
+              gap={2}
+              direction="horizontal"
+              className="justify-content-center align-self-center"
+            >
+              <Link to="/new" className="text-decoration-none">
+                {/* <Button variant="primary">Create</Button> */}
+                <Form
+                  onClick={() => {
+                    console.log("haoasdfasd");
+                  }}
+                >
+                  <Form.Group controlId="formGroupEmail">
+                    <Form.Control
+                      type="text"
+                      className="shadow-none border-focus"
+                      placeholder="Take a note..."
+                    />
+                  </Form.Group>
+                </Form>
+              </Link>
+              <Button
+                onClick={() => setEditTagsModalIsOpen(true)}
+                variant="outline-secondary"
+              >
+                Edit Tags
+              </Button>
+            </Stack>
+          </Col>
+        </Row>
+        {true && (
+          <Form className={`search-form mb-2 ${searchOpen ? "open" : ""}`}>
+            <Row>
+              <Col>
+                <Form.Group controlId="title">
+                  <Form.Label>Title</Form.Label>
                   <Form.Control
                     type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                     className="shadow-none border-focus"
-                    placeholder="Take a note..."
                   />
                 </Form.Group>
-              </Form>
-            </Link>
-            <Button
-              onClick={() => setEditTagsModalIsOpen(true)}
-              variant="outline-secondary"
-            >
-              Edit Tags
-            </Button>
-          </Stack>
-        </Col>
-      </Row>
-      <Form>
-        <Row className="mb-4">
-          <Col>
-            <Form.Group controlId="title">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className="shadow-none border-focus"
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="tags">
-              <Form.Label>Tags</Form.Label>
-              <ReactSelect
-                className="border-focus"
-                value={selectedTags.map((tag) => {
-                  return { label: tag.label, value: tag.id };
-                })}
-                options={availableTags.map((tag) => {
-                  return { label: tag.label, value: tag.id };
-                })}
-                onChange={(tags) => {
-                  setSelectedTags(
-                    tags.map((tag) => {
-                      return { label: tag.label, id: tag.value };
-                    })
-                  );
-                }}
-                isMulti
-              />
-              {/* <ReactSelect
+              </Col>
+              <Col>
+                <Form.Group controlId="tags">
+                  <Form.Label>Tags</Form.Label>
+                  <ReactSelect
+                    className="border-focus"
+                    value={selectedTags.map((tag) => {
+                      return { label: tag.label, value: tag.id };
+                    })}
+                    options={availableTags.map((tag) => {
+                      return { label: tag.label, value: tag.id };
+                    })}
+                    onChange={(tags) => {
+                      setSelectedTags(
+                        tags.map((tag) => {
+                          return { label: tag.label, id: tag.value };
+                        })
+                      );
+                    }}
+                    isMulti
+                  />
+                  {/* <ReactSelect
                 value={selectedTags.map((tag) => {
                   return { label: tag.label, value: tag.id };
                 })}
@@ -149,64 +183,83 @@ export function NoteList({
                   setSelectedTags([{ id: e.value, label: e.label }]);
                 }}
               /> */}
-            </Form.Group>
-          </Col>
-        </Row>
-      </Form>
-      {filteredNotes.length < 1 ? (
-        <div>
-          <div className="text-center h5">Welcome to our Notes Taking App!</div>
-          <div className="p-2">
-            Here's how you can get started:
-            <br /> <strong>1. Create a New Note:- </strong>Click on the "Take a
-            Note" to create a new note. - Enter a title for your note in the
-            designated field. - Write down your notes in the text area provided.
-            - You can also add tags to categorize your notes by clicking on the
-            "Tags" .
-            <br /> <strong>2. Edit and Manage Notes: - </strong>To edit an
-            existing note, click on the note's title or content. - Edit the
-            note's title or content as needed and save your changes. - You can
-            also delete a note by clicking on the "Delete" button.
-            <br /> <strong>3. Tag Management: - </strong>Click on the "Edit
-            Tags" button to manage your tags. - Rename or delete existing tags,
-            and add new ones to help organize your notes.
-            <br /> <strong>4. Search and Filter: - </strong>Use the search title
-            to quickly find notes and tags. - Filter your notes by selecting
-            specific tags to narrow down your search tag.
-            <br /> <strong>5. Account and Sign Out: - </strong>If you have an
-            account, you'll see your email displayed in the top right corner. -
-            To sign out, simply click on the "Sign Out" button. We hope you find
-            our app helpful for keeping your notes organized.
-            <br /> If you have any questions or need assistance, feel free to
-            reach out to our support team.
-            <br /> <strong>Happy note-taking!</strong>
-          </div>
-        </div>
-      ) : (
-        <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
-          {filteredNotes.map((note) => {
-            console.log(note);
-            return (
-              <Col key={note.id}>
-                <NoteCard
-                  id={note.id}
-                  title={note.title}
-                  tags={note.tags}
-                  markdown={note.markdown}
-                  image={note.image}
-                />
+                </Form.Group>
               </Col>
-            );
-          })}
-        </Row>
-      )}
-      <EditTagsModal
-        onUpdateTag={onUpdateTag}
-        onDeleteTag={onDeleteTag}
-        show={editTagsModalIsOpen}
-        handleClose={() => setEditTagsModalIsOpen(false)}
-        availableTags={availableTags}
-      />
+            </Row>
+          </Form>
+        )}
+        {filteredNotes.length < 1 ? (
+          <div>
+            <div className="text-center h5">
+              Welcome to our Notes Taking App!
+            </div>
+            <div className="p-2">
+              Here's how you can get started:
+              <br /> <strong>1. Create a New Note:- </strong>Click on the "Take
+              a Note" to create a new note. - Enter a title for your note in the
+              designated field. - Write down your notes in the text area
+              provided. - You can also add tags to categorize your notes by
+              clicking on the "Tags" .
+              <br /> <strong>2. Edit and Manage Notes: - </strong>To edit an
+              existing note, click on the note's title or content. - Edit the
+              note's title or content as needed and save your changes. - You can
+              also delete a note by clicking on the "Delete" button.
+              <br /> <strong>3. Tag Management: - </strong>Click on the "Edit
+              Tags" button to manage your tags. - Rename or delete existing
+              tags, and add new ones to help organize your notes.
+              <br /> <strong>4. Search and Filter: - </strong>Use the search
+              title to quickly find notes and tags. - Filter your notes by
+              selecting specific tags to narrow down your search tag.
+              <br /> <strong>5. Account and Sign Out: - </strong>If you have an
+              account, you'll see your email displayed in the top right corner.
+              - To sign out, simply click on the "Sign Out" button. We hope you
+              find our app helpful for keeping your notes organized.
+              <br /> If you have any questions or need assistance, feel free to
+              reach out to our support team.
+              <br /> <strong>Happy note-taking!</strong>
+            </div>
+          </div>
+        ) : (
+          <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+            {filteredNotes.map((note) => {
+              console.log(note);
+              return (
+                <Col key={note.id}>
+                  <NoteCard
+                    id={note.id}
+                    title={note.title}
+                    tags={note.tags}
+                    markdown={note.markdown}
+                    image={note.image}
+                  />
+                </Col>
+              );
+            })}
+          </Row>
+        )}
+        <EditTagsModal
+          onUpdateTag={onUpdateTag}
+          onDeleteTag={onDeleteTag}
+          show={editTagsModalIsOpen}
+          handleClose={() => setEditTagsModalIsOpen(false)}
+          availableTags={availableTags}
+        />
+      </div>
+      <div>
+        <footer>
+          <Pagination>
+            <Pagination.First />
+            <Pagination.Prev />
+            <Pagination.Item active>{1}</Pagination.Item>
+
+            <Pagination.Item disabled>{2}</Pagination.Item>
+            <Pagination.Item disabled>{3}</Pagination.Item>
+
+            <Pagination.Next />
+            <Pagination.Last />
+          </Pagination>
+        </footer>
+      </div>
     </div>
   );
 }
