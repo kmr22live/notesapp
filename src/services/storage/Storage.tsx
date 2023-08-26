d5fbbd4187725ffbb6e2863b1d8ccbc1bf2da83e
kmr22live <Kmr22live@gmail.com>
2023-08-26 16:59:56 +0530

    storing notes data to local storage

--- src/services/storage/Storage.tsx
+++ src/services/storage/Storage.tsx
@@ -9,3 +9,41 @@ export const getUserData = (): string | null => {
 export const removeUserData = (): void => {
   localStorage.removeItem("notesappuuid");
 };
+
+export const storeUserData = (data: string): void => {
+  localStorage.setItem("notesappuuid", data);
+};
+
+export const getUserData = (): string | null => {
+  console.log("localdata", localStorage.getItem("notesappuuid"));
+  return localStorage.getItem("notesappuuid");
+};
+
+export const removeUserData = (): void => {
+  localStorage.removeItem("notesappuuid");
+};
+
+export type NotesData = {
+  id: string;
+  title: string;
+  markdown?: string;
+  tagIds?: string[];
+  image?: string | null;
+};
+
+export const setNotesDataLocal = (data: any): void => {
+  localStorage.setItem("notesappuuid_notes", JSON.stringify(data));
+};
+
+export const getNotesDataLocal = (): NotesData[] => {
+  const notesLocalData = localStorage.getItem("notesappuuid_notes");
+  return notesLocalData !== null ? JSON.parse(notesLocalData) : [];
+};
+export const setTagsDataLocal = (data: string): void => {
+  localStorage.setItem("notesappuuid_tags", JSON.stringify(data));
+};
+
+export const getTagsDataLocal = (): NotesData[] => {
+  const notesLocalData = localStorage.getItem("notesappuuid_tags");
+  return notesLocalData !== null ? JSON.parse(notesLocalData) : [];
+};
